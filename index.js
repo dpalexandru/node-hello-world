91f11cb2d679cd4952ae0fb38f14b9effc8e703f
dpalexandru <alexpoian91@hotmail.com>
2025-07-30 15:26:03 +0200

    feat: add condition to check and print parameter

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello Boolean');
+//condizione per visualizzare il parametro passato solo se esiste esiste
+if (process.argv[2]) {
+    console.log(process.argv[2]);
+
+} else {
+    console.log('Non hai iserito nessun parametro');
+}
